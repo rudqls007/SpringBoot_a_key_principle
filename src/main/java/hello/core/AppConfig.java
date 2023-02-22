5cf0ef692723da49211f88074d26284283e00379
rudqls007 <rudqls007@gmail.com>
2023-02-22 20:20:02 +0900

    AppConfig 리펙터링
    중복을 제거하고, 역할에 따른 구현이 보이도록 리팩터링 하자.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberserviceImpl;
@@ -11,11 +12,22 @@ public class AppConfig {
 
     // 생성자 주입
     public MemberService memberservice() {
-      return new MemberserviceImpl(new MemoryMemberRepository());
+      return new MemberserviceImpl(memberRepository());
+    }
+
+    // ctrl + alt + m = 리펙토링
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+    
+    
 
 }
