d94ccde1bc36e76ee3284fb231b9182cc649460b
rudqls007 <rudqls007@gmail.com>
2023-02-22 20:20:02 +0900

    AppConfig MemberApp OrderApp
    스프링으로 전환하기.
    
    @Configuration
    
    @Bean

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,21 +1,43 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberserviceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration // 설정 정보를 담당하는 어노테이션
 public class AppConfig {
 
     // 생성자 주입
+    @Bean   // 각 메서드에 빈 어노테이션을 지정하면 스프링컨테이너에 저장된다.
     public MemberService memberservice() {
-      return new MemberserviceImpl(new MemoryMemberRepository());
+      return new MemberserviceImpl(memberRepository());
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    // ctrl + alt + m = 리펙토링
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
+
+    @Bean
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+    
+    
 
 }
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,23 +7,29 @@ import hello.core.member.MemberserviceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
         AppConfig appconfig = new AppConfig();
-        MemberService memberService = appconfig.memberservice();
-        OrderService orderService = appconfig.orderService();
+//        MemberService memberService = appconfig.memberservice();
+//        OrderService orderService = appconfig.orderService();
 //        MemberService memberService = new MemberserviceImpl(null);
 //        OrderService orderService = new OrderServiceImpl(null, null);
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberservice", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
 
         System.out.println("order = " + order);
